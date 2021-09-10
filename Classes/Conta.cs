546448f54ded73b5ffa6548f343d9cb661950762
Leandro <martins.leandro091@gmail.com>
2021-09-10 13:27:56 -0300

    Desafio .net DIO

--- Classes/Conta.cs
+++ Classes/Conta.cs
@@ -9,14 +9,17 @@ namespace DIO.Bank
 		private double Saldo { get; set; }
 		private double Credito { get; set; }
 		private string Nome { get; set; }
-
+		private double Cripto{ get; set; }
+		private double Investimento{ get; set; }
 		// Métodos
-		public Conta(TipoConta tipoConta, double saldo, double credito, string nome)
+		public Conta(TipoConta tipoConta, double saldo, double credito, string nome, double cripto, double investimento)
 		{
 			this.TipoConta = tipoConta;
 			this.Saldo = saldo;
 			this.Credito = credito;
 			this.Nome = nome;
+			this.Cripto = cripto;
+			this.Investimento = investimento;
 		}
 
 		public bool Sacar(double valorSaque)
@@ -48,12 +51,42 @@ namespace DIO.Bank
             }
 		}
 
+		public bool ComprarCriptos(int unidadeCripto)
+		{
+			double valorCripto = 25 * unidadeCripto;
+			if (this.Saldo - valorCripto < (this.Credito *-1)){
+                Console.WriteLine("Saldo insuficiente!");
+                return false;
+            }
+			this.Saldo -= valorCripto;
+			this.Cripto += unidadeCripto;
+			Console.WriteLine("Saldo atual da conta de {0} é {1}", this.Nome, this.Saldo);
+			Console.WriteLine("Você comprou o valor de {0} em cripto moedas e possui {1} unidades de criptos.", valorCripto, this.Cripto);
+
+			return true;
+		}
+		public bool Investir(double valorInvestimento)
+		{
+			if (this.Saldo - valorInvestimento < (this.Credito *-1)){
+                Console.WriteLine("Saldo insuficiente!");
+                return false;
+            }
+			this.Saldo -= valorInvestimento;
+			this.Investimento += valorInvestimento;
+
+			Console.WriteLine("Valor investido é de {0} ", this.Investimento);
+			Console.WriteLine("Valor do saldo da conta é de {0} ", this.Saldo);
+
+			return true;
+		}
         public override string ToString()
 		{
             string retorno = "";
             retorno += "TipoConta " + this.TipoConta + " | ";
             retorno += "Nome " + this.Nome + " | ";
             retorno += "Saldo " + this.Saldo + " | ";
+			retorno += "Cripto Moedas " + this.Cripto + " | ";
+			retorno += "Investimentos " + this.Investimento + " | ";
             retorno += "Crédito " + this.Credito;
 			return retorno;
 		}
--- Program.cs
+++ Program.cs
@@ -29,6 +29,12 @@ namespace DIO.Bank
 					case "5":
 						Depositar();
 						break;
+					case "6":
+						Investir();
+						break;
+					case "7":
+						ComprarCriptos();
+						break;
                     case "C":
 						Console.Clear();
 						break;
@@ -96,10 +102,18 @@ namespace DIO.Bank
 			Console.Write("Digite o crédito: ");
 			double entradaCredito = double.Parse(Console.ReadLine());
 
+			Console.Write("Digite as unidades de criptos: ");
+			double entradaCripto = double.Parse(Console.ReadLine());
+
+			Console.Write("Digite o dinheiro investido: ");
+			double entradaInvestir = double.Parse(Console.ReadLine());
+
 			Conta novaConta = new Conta(tipoConta: (TipoConta)entradaTipoConta,
 										saldo: entradaSaldo,
 										credito: entradaCredito,
-										nome: entradaNome);
+										nome: entradaNome,
+										cripto: entradaCripto,
+										investimento: entradaInvestir);
 
 			listContas.Add(novaConta);
 		}
@@ -121,9 +135,47 @@ namespace DIO.Bank
 				Console.WriteLine(conta);
 			}
 		}
+		private static void Investir()
+		{
+			try{
+				Console.WriteLine("Investir");
 
+				Console.WriteLine();
+
+				Console.Write("Digite o número da conta: ");
+				int indiceConta = int.Parse(Console.ReadLine());
+				
+				Console.WriteLine("Digite quanto deseja investir: ");
+				double valorInvestimento = double.Parse(Console.ReadLine());
+
+            	listContas[indiceConta].Investir(valorInvestimento);
+			}catch(ArgumentOutOfRangeException){	
+				Console.WriteLine();
+				Console.Write("A conta selecionada não existe.");
+			}
+		}
+		private static void ComprarCriptos()
+		{		
+			try{	
+				Console.WriteLine("Comprar cripto moeda");
+
+				Console.WriteLine();
+			
+				Console.Write("Digite o número da conta: ");
+				int indiceConta = int.Parse(Console.ReadLine());
+
+				Console.Write("Uma unidade de cripto custa 25 reais, quantas deseja comprar?: ");
+				int unidadeCripto = int.Parse(Console.ReadLine());
+			
+            	listContas[indiceConta].ComprarCriptos(unidadeCripto);
+			}catch(ArgumentOutOfRangeException){	
+				Console.WriteLine();
+				Console.Write("A conta selecionada não existe.");
+			}
+		}
 		private static string ObterOpcaoUsuario()
 		{
+			Console.ForegroundColor = ConsoleColor.Green; 
 			Console.WriteLine();
 			Console.WriteLine("DIO Bank a seu dispor!!!");
 			Console.WriteLine("Informe a opção desejada:");
@@ -133,6 +185,8 @@ namespace DIO.Bank
 			Console.WriteLine("3- Transferir");
 			Console.WriteLine("4- Sacar");
 			Console.WriteLine("5- Depositar");
+			Console.WriteLine("6- Investir");
+			Console.WriteLine("7- Comprar Criptos");
             Console.WriteLine("C- Limpar Tela");
 			Console.WriteLine("X- Sair");
 			Console.WriteLine();
